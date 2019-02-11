07fc120c8dbbb96de84bc29fd9239d0f04713617
xiaoniubei <15210690703@163.com>
2019-02-11 14:37:40 +0800

    分页初始化修改

--- public/default/js/jqPaginator.js
+++ public/default/js/jqPaginator.js
@@ -240,11 +240,11 @@
 
     $.jqPaginator.defaultOptions = {
         wrapper: '',
-        first: '<li class="first"><a href="javascript:;">第一页</a></li>',
-        prev: '<li class="prev"><a href="javascript:;">上一页</a></li>',
-        next: '<li class="next"><a href="javascript:;">下一页</a></li>',
-        last: '<li class="last"><a href="javascript:;">最后一页</a></li>',
-        page: '<li class="page"><a href="javascript:;">{{page}}</a></li>',
+        first: '<li class="page-item"><a class="page-link" href="javascript:;">第一页</a></li>',
+        prev: '<li class="page-item"><a class="page-link" href="javascript:;">上一页</a></li>',
+        next: '<li class="page-item"><a class="page-link" href="javascript:;">下一页</a></li>',
+        last: '<li class="page-item"><a class="page-link" href="javascript:;">最后一页</a></li>',
+        page: '<li class="page-item"><a class="page-link" href="javascript:;">{{page}}</a></li>',
         totalPages: 0,
         totalCounts: 0,
         pageSize: 0,
